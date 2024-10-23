d2b79b2b6737b5cd2c70e046bceb5f0850ac1ed7
Daniel Bailey <dan@nexcade.ai>
2024-10-23 12:00:20 +0100

    Initial commit: Clean, Apple-style website

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
- 
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav a');
+    
+    for (const link of links) {
+        link.addEventListener('click', clickHandler);
+    }
+    
+    function clickHandler(e) {
+        e.preventDefault();
+        const href = this.getAttribute('href');
+        const offsetTop = document.querySelector(href).offsetTop;
+        
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+});
